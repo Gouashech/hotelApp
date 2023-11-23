d73b4930220ab20ebb4c5710c18653be9f827e56
Gouashech <149411918+Gouashech@users.noreply.github.com>
2023-11-23 15:41:05 +0200

    Update store.jsx

--- src/Components/store.jsx
+++ src/Components/store.jsx
@@ -4,16 +4,13 @@ import createSagaMiddleware from 'redux-saga';
 import { reducer } from './reducers';
 import { rootSaga } from './sagas';
 
-// Створення middleware для Redux Saga
 const sagaMiddleware = createSagaMiddleware();
 
-// Створення сховища з використанням configureStore
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
 
-// Запуск саги
 sagaMiddleware.run(rootSaga);
 
 export default store;
