8e2b6a455d8d352201a9e051cae65cc23f95ed51
Gouashech <149411918+Gouashech@users.noreply.github.com>
2023-11-27 19:22:47 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Authorization from './Pages/Authorization';
-import LandingPage from './Pages/MainLayout';
-import { Provider } from 'react-redux'; 
-import store from './Components/store';
-
-
-
-
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Wrapper from './components/Wrapper';
+import './App.scss';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Authorization />} />
-          <Route index path="/Home" element={<Home />} />
-          <Route index path="/LandingPage" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Wrapper />
+    </BrowserRouter>
   );
 }
 
